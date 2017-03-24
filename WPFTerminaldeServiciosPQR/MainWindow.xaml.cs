4ee77d7e98b9bfce0903210b6f223807132a5591
RolandGarzon <RolandGarzon@COORDINADORGI>
2017-03-24 17:31:28 -0500

    Add project files.

---
+++ WPFTerminaldeServiciosPQR/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace WPFTerminaldeServiciosPQR
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void btnPazSalvo_Click(object sender, RoutedEventArgs e)
+        {
+            generarCupon wingenerarCupon = new generarCupon();
+            wingenerarCupon.Show();
+            this.Close();
+        }
+    }
+}
